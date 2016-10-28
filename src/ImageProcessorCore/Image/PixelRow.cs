a79221e138d728945382f7a5d268ccb100d5901d
dirk <dirk@git.imagemagick.org>
2016-10-28 23:00:15 +0200

    Fixed header.

--- src/ImageProcessorCore/Image/PixelRow.cs
+++ src/ImageProcessorCore/Image/PixelRow.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="PixelAccessor.cs" company="James Jackson-South">
+﻿// <copyright file="PixelRow.cs" company="James Jackson-South">
 // Copyright (c) James Jackson-South and contributors.
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
